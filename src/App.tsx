e8b9f087f758c2b276adedffce6416345b101cb5
Giacomo Salsano <giacomosalsano@hotmail.com>
2025-08-23 16:56:58 -0300

    Primo commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="m-12 p-3 text-3xl font-bold text-red-500 underline">
+        Hello World
+      </h1>
+    </>
+  );
+}
+
+export default App;

5d2133bcf21cb1301a3968ea98486256cd7e495c
Giacomo Salsano <giacomosalsano@hotmail.com>
2025-08-24 15:38:09 -0300

    refactor: drawer componentization

--- src/components/actions/patient-details/index.tsx
+++ src/components/actions/patient-details/index.tsx
@@ -1,18 +1,12 @@
 import { Button } from "@/components/ui/button";
-import {
-  Drawer,
-  DrawerContent,
-  DrawerDescription,
-  DrawerHeader,
-  DrawerTitle,
-  DrawerTrigger,
-} from "@/components/ui/drawer";
 import type { Patient } from "@/modules/patient/types/patient";
 import { EyeIcon } from "lucide-react";
 
 import { columns } from "@/components/actions/patient-details/components/columns";
 import { PatientDetailsTable } from "@/components/actions/patient-details/components/patient-details-table";
 import { PatientInfoBox } from "@/components/patient-info-box";
+import { DrawerComponent } from "@/components/ui/components/drawer-component";
+import { useMemo } from "react";
 
 interface PatientDetailsProps {
   patient: Patient;
@@ -21,38 +15,39 @@ interface PatientDetailsProps {
 export const PatientDetails = ({ patient }: PatientDetailsProps) => {
   const patientHasParameters = patient.parameters?.length > 0;
 
+  const patientDetailsTrigger = useMemo(() => {
+    return (
+      <Button variant="outline" size="sm">
+        <EyeIcon className="h-4 w-4" />
+      </Button>
+    );
+  }, []);
+
+  const patientDetailsContent = useMemo(() => {
+    return (
+      <div className="flex min-w-6xl flex-col gap-4 space-y-4 overflow-y-auto px-4 pb-4 text-center">
+        <PatientInfoBox patient={patient} />
+        {patientHasParameters ? (
+          <div className="w-full items-center justify-center">
+            <PatientDetailsTable columns={columns} data={patient.parameters} />
+          </div>
+        ) : (
+          <div>
+            <p className="text-muted-foreground">
+              This patient has no parameters
+            </p>
+          </div>
+        )}
+      </div>
+    );
+  }, []);
+
   return (
-    <Drawer>
-      <DrawerTrigger asChild>
-        <Button variant="outline" size="sm">
-          <EyeIcon className="h-4 w-4" />
-        </Button>
-      </DrawerTrigger>
-      <DrawerContent className="mx-auto w-full items-center justify-center">
-        <DrawerHeader>
-          <DrawerTitle>Patient Details</DrawerTitle>
-          <DrawerDescription>
-            Here you can see the patient details.
-          </DrawerDescription>
-        </DrawerHeader>
-        <div className="flex min-w-6xl flex-col gap-4 space-y-4 overflow-y-auto px-4 pb-4 text-center">
-          <PatientInfoBox patient={patient} />
-          {patientHasParameters ? (
-            <div className="w-full items-center justify-center">
-              <PatientDetailsTable
-                columns={columns}
-                data={patient.parameters}
-              />
-            </div>
-          ) : (
-            <div>
-              <p className="text-muted-foreground">
-                This patient has no parameters
-              </p>
-            </div>
-          )}
-        </div>
-      </DrawerContent>
-    </Drawer>
+    <DrawerComponent
+      trigger={patientDetailsTrigger}
+      title="Patient Details"
+      description="Here you can see the patient details."
+      children={patientDetailsContent}
+    />
   );
 };
---
+++ src/components/ui/components/drawer-component.tsx
@@ -0,0 +1,37 @@
+import {
+  Drawer,
+  DrawerContent,
+  DrawerDescription,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "../drawer";
+
+interface DrawerComponentProps {
+  trigger: React.ReactNode;
+  title: string;
+  description: string;
+  children: React.ReactNode;
+}
+
+export function DrawerComponent({
+  trigger,
+  title,
+  description,
+  children,
+}: DrawerComponentProps) {
+  return (
+    <Drawer>
+      <DrawerTrigger asChild>{trigger}</DrawerTrigger>
+      <DrawerContent className="mx-auto w-full items-center justify-center">
+        <DrawerHeader>
+          <DrawerTitle>{title}</DrawerTitle>
+          <DrawerDescription>{description}</DrawerDescription>
+        </DrawerHeader>
+        <div className="flex min-w-6xl flex-col gap-4 space-y-4 overflow-y-auto px-4 pb-4 text-center">
+          {children}
+        </div>
+      </DrawerContent>
+    </Drawer>
+  );
+}
